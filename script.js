8b29d99de8e109800233dc886b3909b0c6d5939f
5nahalf <5nahalf@comcast.net>
2015-04-29 14:48:40 -0500

    made stuff work

--- script.js
+++ script.js
@@ -1,71 +1,71 @@
 var apikey = 'fbf1f9033c363564ebbbfddfee9708bc18e08cb0'; // Put your API key here
 var game;
 var i = 0;
-var resultsGame = [];
 var currentPos = 0;
 var currentChild;
+var currentDot;
 // Use this function to do stuff with your results. 
 // It is called after 'search' is executed.
 function searchCallback(results) {
- 
- 
+        $(".middle").empty();
+		$(".row").empty();
+		$(".jack").empty();
     for(i = 0; i <= 7; i++){
-    	$("#wait").empty();
     	var picture = results[i].image.medium_url;
     	var deck = results[i].deck;
     	var game = results[i].site_detail_url;
-    	resultsGame.push(results[i]);
-    	// display(results[i]);
-    	    	$(".middle").append("<div class='hidden container well col-md-4 col-xs-12' id='mainWindow'><div class='hidden-sm hidden-xs' id='gameImage'><img src='" + picture + "'/></div><div class='outer' id='gameTitle'><p class='lead text-center'>" + results[i].name + "</p></div><p id='textHidden'>" + results[i].deck + "<br><a href='" + game + "' target='_blank'>Take me there</a></p><button class='btn-sm btn-primary' id='removeGame'>Remove Game</button></div>");
+    	    $(".middle").append("<div class='hidden container well col-md-4 col-xs-12' id='mainWindow'><div class='hidden-sm hidden-xs' id='gameImage'><img src='" + picture + "'/></div><div class='outer' id='gameTitle'><p class='lead text-center'>" + results[i].name + "</p></div><p id='textHidden'>" + results[i].deck + "<br><a href='" + game + "' target='_blank'>Take me there</a></p></div>");
 	}
-	console.log($(".middle").children().first());
+	$(".row").append('<div class="dot"></div><div class="dot"></div><div class="dot"></div><div class="dot"></div><div class="dot"></div><div class="dot"></div><div class="dot"></div><div class="dot"></div>');
 	display();
 }
-
 function display(){
 	currentChild = $(".middle").children().first();
 	currentChild.removeClass("hidden");
+	currentDot = $(".row").children().first();
+	currentDot.addClass("rowDot");
 }
-
-
 function right(){
 	currentChild.addClass("hidden");
-	if(currentPos == 7){
+	currentDot.removeClass("rowDot");
+	currentPos++;
+	if(currentPos == 8){
 		currentChild = $(".middle").children().first();
 		currentPos = 0;
+		currentDot = $(".row").children().first();
 	} else {
 	currentChild = currentChild.next();
+	currentDot = currentDot.next();
 }
 	currentChild.removeClass("hidden");
-	currentPos++;
+	currentDot.addClass("rowDot");
 }
 function left(){
 	currentChild.addClass("hidden");
+	currentDot.removeClass("rowDot");
 	if(currentPos == 0){
 		currentChild = $(".middle").children().last();
-		currentPos = 7;
+		currentPos = 8;
+		currentDot = $(".row").children().last();
 	} else {
 	currentChild = currentChild.prev();
+	currentDot = currentDot.prev();
 }
 	currentChild.removeClass("hidden");
+	currentDot.addClass("rowDot");
 	currentPos--;
 }
-
-
 $(document).ready(function() {
 	$(".searchBtn").on("click", function(){
+		$(".jack").empty();
 		event.preventDefault();
 		game = $("#searchField").val();
 		search(game);
-		$("#searchBox").append("<p id='wait'>Please wait.....</p>");
-		
+		$("#searchBox").append("<div class='jack'><p id='wait'>Please wait.....</p></div>");
 	});
 	$(".clearList").on("click", function(){
-		$("#returnGame").fadeOut();
-
-	});
-	$("#returnGame").on("click", "#removeGame", function(){
-		$(this).parent().fadeOut();
+		$(".middle").empty();
+		$(".row").empty();	
 	});
 	$(".pull2").on("click", function(){
 		right();
@@ -73,11 +73,6 @@ $(document).ready(function() {
 	$(".pull1").on("click", function(){
 		left();
 	});
-	
-
-
-	// Start the search here!
-	
 });
 
 // HELPER FUNCTION
